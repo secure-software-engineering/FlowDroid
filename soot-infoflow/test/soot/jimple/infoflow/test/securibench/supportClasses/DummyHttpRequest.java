bec2ba4d76475a5455b44434e18698ab70078b31
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2020-02-29 17:03:25 +0100

    Revert "Merge pull request #238 from MarcMil/develop-fix"
    This reverts commit 2b28ab4485f47fe86ea296e8334e0f59c7ebb8f0, reversing
    changes made to 1b0af4f2dff4ac27ffb5f2787f1fca9ea6c7ba32.

--- soot-infoflow/test/soot/jimple/infoflow/test/securibench/supportClasses/DummyHttpRequest.java
+++ soot-infoflow/test/soot/jimple/infoflow/test/securibench/supportClasses/DummyHttpRequest.java
@@ -24,7 +24,6 @@ import javax.servlet.AsyncContext;
 import javax.servlet.DispatcherType;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -32,7 +31,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpUpgradeHandler;
 import javax.servlet.http.Part;
 
 public class DummyHttpRequest implements HttpServletRequest {
@@ -426,24 +424,6 @@ public class DummyHttpRequest implements HttpServletRequest {
 	public int getRemotePort() {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	@Override
-	public long getContentLengthLong() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String changeSessionId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends HttpUpgradeHandler> T upgrade(Class<T> handlerClass) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
