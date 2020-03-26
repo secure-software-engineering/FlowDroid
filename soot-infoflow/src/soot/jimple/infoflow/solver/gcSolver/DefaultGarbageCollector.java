2922f3e7a5ea4cac5165c55cdd55cbe701af3769
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2020-03-26 11:25:57 +0100

    better control over garbage collector triggers

--- soot-infoflow/src/soot/jimple/infoflow/solver/gcSolver/DefaultGarbageCollector.java
+++ soot-infoflow/src/soot/jimple/infoflow/solver/gcSolver/DefaultGarbageCollector.java
@@ -19,16 +19,48 @@ import soot.util.ConcurrentHashMultiMap;
  */
 public class DefaultGarbageCollector<N, D> extends AbstractGarbageCollector<N, D> {
 
+	/**
+	 * Possible triggers when to start garbage collection
+	 * 
+	 * @author Steven Arzt
+	 *
+	 */
+	public enum GarbageCollectionTrigger {
+
+	/**
+	 * Garbage collection is triggered immediately when a new edge has been
+	 * propagated
+	 */
+	Immediate,
+
+	/**
+	 * Start garbage collection after the method threshold has been reached
+	 */
+	MethodThreshold,
+
+	/**
+	 * Start garbage collection after the edge threshold has been reached
+	 */
+	EdgeThreshold
+
+	}
+
 	private ConcurrentCountingMap<SootMethod> jumpFnCounter = new ConcurrentCountingMap<>();
 	private final Set<SootMethod> gcScheduleSet = new ConcurrentHashSet<>();
 	private final AtomicInteger gcedMethods = new AtomicInteger();
 	private final AtomicInteger gcedEdges = new AtomicInteger();
+	private final AtomicInteger edgeCounterForThreshold = new AtomicInteger();
+	private GarbageCollectionTrigger trigger = GarbageCollectionTrigger.Immediate;
 
 	/**
 	 * The number of methods to collect as candidates for garbage collection, before
 	 * halting the taint propagation and actually cleaning them up
 	 */
-	private int GC_THRESHOLD = 0;
+	private int methodThreshold = 0;
+	/**
+	 * Wait for at least this number of edges before starting the garbage collection
+	 */
+	private int edgeThreshold = 0;
 
 	public DefaultGarbageCollector(BiDiInterproceduralCFG<N, SootMethod> icfg,
 			ConcurrentHashMultiMap<SootMethod, PathEdge<N, D>> jumpFunctions) {
@@ -39,9 +71,8 @@ public class DefaultGarbageCollector<N, D> extends AbstractGarbageCollector<N, D
 	public void notifyEdgeSchedule(PathEdge<N, D> edge) {
 		SootMethod sm = icfg.getMethodOf(edge.getTarget());
 		jumpFnCounter.increment(sm);
-		synchronized (gcScheduleSet) {
-			gcScheduleSet.add(sm);
-		}
+		gcScheduleSet.add(sm);
+		edgeCounterForThreshold.incrementAndGet();
 	}
 
 	@Override
@@ -51,7 +82,15 @@ public class DefaultGarbageCollector<N, D> extends AbstractGarbageCollector<N, D
 
 	@Override
 	public void gc() {
-		if (gcScheduleSet.size() > GC_THRESHOLD) {
+		// Check our various triggers for garbage collection
+		boolean gc = trigger == GarbageCollectionTrigger.Immediate;
+		gc |= trigger == GarbageCollectionTrigger.MethodThreshold && gcScheduleSet.size() > methodThreshold;
+		gc |= trigger == GarbageCollectionTrigger.EdgeThreshold && edgeCounterForThreshold.get() > edgeThreshold;
+
+		// Perform the garbage collection if required
+		if (gc) {
+			edgeCounterForThreshold.set(0);
+
 			// Get the methods for which no propagation tasks are scheduled right now. This
 			// set is approximate, because other threads may add new tasks while we're
 			// collecting our GC candidates.
@@ -69,7 +108,7 @@ public class DefaultGarbageCollector<N, D> extends AbstractGarbageCollector<N, D
 				}
 
 				// Clean up the methods
-				if (toRemove.size() > GC_THRESHOLD) {
+				if (toRemove.size() > methodThreshold) {
 					for (Iterator<SootMethod> it = toRemove.iterator(); it.hasNext();) {
 						SootMethod sm = it.next();
 						Set<PathEdge<N, D>> oldFunctions = jumpFunctions.get(sm);
@@ -112,6 +151,38 @@ public class DefaultGarbageCollector<N, D> extends AbstractGarbageCollector<N, D
 	@Override
 	public int getGcedEdges() {
 		return gcedEdges.get();
+	}
+
+	/**
+	 * Sets the number of methods for which edges must have been added before
+	 * garbage collection is started
+	 * 
+	 * @param threshold The threshold of new methods required to trigger garbage
+	 *                  collection
+	 */
+	public void setMethodThreshold(int threshold) {
+		this.methodThreshold = threshold;
+	}
+
+	/**
+	 * Sets the minimum number of edges that shall be propagated before triggering
+	 * the garbage collection
+	 * 
+	 * @param threshold The minimum number of edges that shall be propagated before
+	 *                  triggering the garbage collection
+	 */
+	public void setEdgeThreshold(int threshold) {
+		this.edgeThreshold = threshold;
+	}
+
+	/**
+	 * Set the trigger that defines when garbage collection shall be started
+	 * 
+	 * @param trigger The trigger that defines when garbage collection shall be
+	 *                started
+	 */
+	public void setTrigger(GarbageCollectionTrigger trigger) {
+		this.trigger = trigger;
 	}
 
 }
