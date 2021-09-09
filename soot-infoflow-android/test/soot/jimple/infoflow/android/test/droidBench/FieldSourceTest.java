d027e756082d2db7f132255ece85922a258ec659
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2021-09-09 14:17:58 +0200

    disabled test case due to missing APK file

--- soot-infoflow-android/test/soot/jimple/infoflow/android/test/droidBench/FieldSourceTest.java
+++ soot-infoflow-android/test/soot/jimple/infoflow/android/test/droidBench/FieldSourceTest.java
@@ -1,17 +1,20 @@
 package soot.jimple.infoflow.android.test.droidBench;
 
+import java.io.IOException;
+
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xmlpull.v1.XmlPullParserException;
-import soot.jimple.infoflow.results.InfoflowResults;
 
-import java.io.IOException;
+import soot.jimple.infoflow.results.InfoflowResults;
 
 public class FieldSourceTest extends JUnitTests {
 
-    @Test
-    public void runTestFlowSensitivity1() throws IOException, XmlPullParserException {
-        InfoflowResults res = analyzeAPKFile("FieldSource/FieldSourceTest.apk");
-        Assert.assertNotNull(res);
-    }
+	@Test
+	@Ignore("APK file missing")
+	public void runTestFlowSensitivity1() throws IOException, XmlPullParserException {
+		InfoflowResults res = analyzeAPKFile("FieldSource/FieldSourceTest.apk");
+		Assert.assertNotNull(res);
+	}
 }
