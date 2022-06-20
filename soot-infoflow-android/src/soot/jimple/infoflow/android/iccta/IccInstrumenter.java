4c84c4845f47e1f99b72b17422d2e8fddfde113f
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2022-06-20 11:32:54 +0200

    style fix

--- soot-infoflow-android/src/soot/jimple/infoflow/android/iccta/IccInstrumenter.java
+++ soot-infoflow-android/src/soot/jimple/infoflow/android/iccta/IccInstrumenter.java
@@ -1,12 +1,12 @@
 package soot.jimple.infoflow.android.iccta;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.Map;
-import java.util.HashMap;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +14,12 @@ import org.slf4j.LoggerFactory;
 import soot.Body;
 import soot.Local;
 import soot.LocalGenerator;
+import soot.RefType;
 import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
 import soot.Unit;
 import soot.Value;
-import soot.RefType;
 import soot.jimple.Jimple;
 import soot.jimple.Stmt;
 import soot.jimple.infoflow.android.entryPointCreators.components.ComponentEntryPointCollection;
@@ -106,18 +106,18 @@ public class IccInstrumenter implements PreAnalysisHandler {
 		logger.info("Launching Messenger Transformer...");
 
 		Chain<SootClass> applicationClasses = Scene.v().getApplicationClasses();
-		Map<SootClass,Map<Value,String>> appClasses = new HashMap<>();
-		Map<String,String> handlerInner= new HashMap<>();
+		Map<SootClass, Map<Value, String>> appClasses = new HashMap<>();
+		Map<String, String> handlerInner = new HashMap<>();
 
 		for (Iterator<SootClass> iter = applicationClasses.snapshotIterator(); iter.hasNext();) {
 			SootClass sootClass = iter.next();
-			Map<Value,String> handlerClass= new HashMap<>();
+			Map<Value, String> handlerClass = new HashMap<>();
 			// We copy the list of methods to emulate a snapshot iterator which
 			// doesn't exist for methods in Soot
 			List<SootMethod> methodCopyList = new ArrayList<>(sootClass.getMethods());
 			for (SootMethod sootMethod : methodCopyList) {
 				if (sootMethod.isConcrete()) {
-					final Body body = sootMethod.retrieveActiveBody();				
+					final Body body = sootMethod.retrieveActiveBody();
 
 					// Mark the method as processed
 					if (!processedMethods.add(sootMethod))
@@ -126,15 +126,18 @@ public class IccInstrumenter implements PreAnalysisHandler {
 					for (Iterator<Unit> unitIter = body.getUnits().snapshotIterator(); unitIter.hasNext();) {
 						Unit unit = unitIter.next();
 						Stmt stmt = (Stmt) unit;
-						//+++ collect handler fields signature	
+						// +++ collect handler fields signature
 						if (stmt.containsFieldRef())
-							if(stmt.getFieldRef().getType() instanceof RefType)
-								if(((RefType)stmt.getFieldRef().getType()).getSootClass().getName().contains("android.os.Handler")) {
-									if(stmt.getUseBoxes().size() > 1)
-										handlerInner.putIfAbsent(stmt.getFieldRef().getField().getSignature(),stmt.getUseBoxes().get(1).getValue().getType().toString());
+							if (stmt.getFieldRef().getType() instanceof RefType)
+								if (((RefType) stmt.getFieldRef().getType()).getSootClass().getName()
+										.contains("android.os.Handler")) {
+									if (stmt.getUseBoxes().size() > 1)
+										handlerInner.putIfAbsent(stmt.getFieldRef().getField().getSignature(),
+												stmt.getUseBoxes().get(1).getValue().getType().toString());
 									// use value as locator
-									handlerClass.put(stmt.getDefBoxes().get(0).getValue(),stmt.getFieldRef().getField().getSignature());
-									}													
+									handlerClass.put(stmt.getDefBoxes().get(0).getValue(),
+											stmt.getFieldRef().getField().getSignature());
+								}
 					}
 
 				}
@@ -144,37 +147,38 @@ public class IccInstrumenter implements PreAnalysisHandler {
 			appClasses.putIfAbsent(sootClass, handlerClass);
 
 		}
-		//instrument the outerclass
-		for(SootClass sc : appClasses.keySet())
-			if(sc!=null)
-				generateSendMessage(sc,appClasses,handlerInner);
+		// instrument the outerclass
+		for (SootClass sc : appClasses.keySet()) {
+			if (sc != null)
+				generateSendMessage(sc, appClasses.get(sc), handlerInner);
+		}
 	}
 
-	public void generateSendMessage(SootClass sootClass,Map<SootClass,Map<Value,String>> appClasses,Map<String,String> handlerInner) {
-		
-		if(appClasses.isEmpty() || handlerInner.isEmpty())
+	public void generateSendMessage(SootClass sootClass, Map<Value, String> appClasses,
+			Map<String, String> handlerInner) {
+		if (appClasses.isEmpty() || handlerInner.isEmpty())
 			return;
 		List<SootMethod> methodCopyList = new ArrayList<>(sootClass.getMethods());
 		for (SootMethod sootMethod : methodCopyList) {
 			if (sootMethod.isConcrete()) {
 				final Body body = sootMethod.retrieveActiveBody();
 				final LocalGenerator lg = Scene.v().createLocalGenerator(body);
-				
+
 				for (Iterator<Unit> unitIter = body.getUnits().snapshotIterator(); unitIter.hasNext();) {
 					Unit unit = unitIter.next();
 					Stmt stmt = (Stmt) unit;
 					if (stmt.containsInvokeExpr()) {
 						SootMethod callee = stmt.getInvokeExpr().getMethod();
-						
+
 						// For sendMessage(), we directly call the respective handler.handleMessage()
 						if (callee == smMessengerSend || callee == smSendMessage) {
-							//collect the value for sendMessage()
-							String hc = appClasses.get(sootClass).get(stmt.getInvokeExpr().getUseBoxes().get(1).getValue());
-							Set<SootClass> handlers = MessageHandler.v().getAllHandlers();								
+							// collect the value for sendMessage()
+							String hc = appClasses.get(stmt.getInvokeExpr().getUseBoxes().get(1).getValue());
+							Set<SootClass> handlers = MessageHandler.v().getAllHandlers();
 							for (SootClass handler : handlers) {
-								// matching the handler and its signature	
-								if(hc != null && handlerInner.get(hc) == handler.getName()) {
-									Local handlerLocal = lg.generateLocal(handler.getType());													
+								// matching the handler and its signature
+								if (hc != null && handlerInner.get(hc) == handler.getName()) {
+									Local handlerLocal = lg.generateLocal(handler.getType());
 									SootMethod hmMethod = handler.getMethod("void handleMessage(android.os.Message)");
 									Unit callHMU = Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(
 											handlerLocal, hmMethod.makeRef(), stmt.getInvokeExpr().getArg(0)));
@@ -182,17 +186,16 @@ public class IccInstrumenter implements PreAnalysisHandler {
 									body.getUnits().insertAfter(callHMU, stmt);
 									instrumentedUnits.put(body, callHMU);
 									break;
-								}			
+								}
 							}
 						}
-					}	
-												
-					
+					}
+
 				}
 
 			}
 		}
-		
+
 	}
 
 	@Override
