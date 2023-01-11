9eea9ac94abe47ae9a10f383cb197b084dec11ab
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2023-01-11 17:25:43 +0100

    added a new scheduling strategy

--- soot-infoflow/src/soot/jimple/infoflow/solver/fastSolver/DefaultSchedulingStrategy.java
+++ soot-infoflow/src/soot/jimple/infoflow/solver/fastSolver/DefaultSchedulingStrategy.java
@@ -14,6 +14,9 @@ public class DefaultSchedulingStrategy<N, D extends FastSolverLinkedNode<D, N>, 
 
 	protected final IFDSSolver<N, D, I> solver;
 
+	/**
+	 * Strategy that schedules each edge individually, potentially in a new thread
+	 */
 	public final ISchedulingStrategy<N, D> EACH_EDGE_INDIVIDUALLY = new ISchedulingStrategy<N, D>() {
 
 		@Override
@@ -53,6 +56,10 @@ public class DefaultSchedulingStrategy<N, D extends FastSolverLinkedNode<D, N>, 
 
 	};
 
+	/**
+	 * Strategy that schedules each edge that crosses a method boundary, but
+	 * processes all edges inside the same method locally in the same task
+	 */
 	public final ISchedulingStrategy<N, D> EACH_METHOD_INDIVIDUALLY = new ISchedulingStrategy<N, D>() {
 
 		@Override
@@ -86,6 +93,44 @@ public class DefaultSchedulingStrategy<N, D extends FastSolverLinkedNode<D, N>, 
 				boolean isUnbalancedReturn) {
 			solver.propagate(sourceVal, target, targetVal, relatedCallSite, isUnbalancedReturn,
 					ScheduleTarget.EXECUTOR);
+		};
+
+	};
+
+	/**
+	 * Strategy that never schedules a new task, i.e., processes all edges locally
+	 * in same task
+	 */
+	public final ISchedulingStrategy<N, D> ALL_EDGES_LOCALLY = new ISchedulingStrategy<N, D>() {
+
+		@Override
+		public void propagateInitialSeeds(D sourceVal, N target, D targetVal, N relatedCallSite,
+				boolean isUnbalancedReturn) {
+			solver.propagate(sourceVal, target, targetVal, relatedCallSite, isUnbalancedReturn, ScheduleTarget.LOCAL);
+		}
+
+		@Override
+		public void propagateNormalFlow(D sourceVal, N target, D targetVal, N relatedCallSite,
+				boolean isUnbalancedReturn) {
+			solver.propagate(sourceVal, target, targetVal, relatedCallSite, isUnbalancedReturn, ScheduleTarget.LOCAL);
+		}
+
+		@Override
+		public void propagateCallFlow(D sourceVal, N target, D targetVal, N relatedCallSite,
+				boolean isUnbalancedReturn) {
+			solver.propagate(sourceVal, target, targetVal, relatedCallSite, isUnbalancedReturn, ScheduleTarget.LOCAL);
+		}
+
+		@Override
+		public void propagateCallToReturnFlow(D sourceVal, N target, D targetVal, N relatedCallSite,
+				boolean isUnbalancedReturn) {
+			solver.propagate(sourceVal, target, targetVal, relatedCallSite, isUnbalancedReturn, ScheduleTarget.LOCAL);
+		}
+
+		@Override
+		public void propagateReturnFlow(D sourceVal, N target, D targetVal, N relatedCallSite,
+				boolean isUnbalancedReturn) {
+			solver.propagate(sourceVal, target, targetVal, relatedCallSite, isUnbalancedReturn, ScheduleTarget.LOCAL);
 		};
 
 	};
