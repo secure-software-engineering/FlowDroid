3474096a11c4957ecef3f56627f07c75c2a47465
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2023-04-03 18:12:49 +0200

    added more logging

--- soot-infoflow/src/soot/jimple/infoflow/river/ConditionalFlowPostProcessor.java
+++ soot-infoflow/src/soot/jimple/infoflow/river/ConditionalFlowPostProcessor.java
@@ -1,7 +1,11 @@
 package soot.jimple.infoflow.river;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import soot.jimple.infoflow.InfoflowManager;
 import soot.jimple.infoflow.handlers.PostAnalysisHandler;
 import soot.jimple.infoflow.results.DataFlowResult;
@@ -10,43 +14,41 @@ import soot.jimple.infoflow.results.ResultSinkInfo;
 import soot.jimple.infoflow.solver.cfg.IInfoflowCFG;
 import soot.jimple.infoflow.sourcesSinks.definitions.SourceSinkCondition;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
- * Checks whether conditional sinks met their condition and filter those which did not.
+ * Checks whether conditional sinks met their condition and filter those which
+ * did not.
  *
  * @author Tim Lange
  */
 public class ConditionalFlowPostProcessor implements PostAnalysisHandler {
-    protected final Logger logger = LoggerFactory.getLogger(getClass());
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
-    InfoflowManager manager;
+	InfoflowManager manager;
 
-    public ConditionalFlowPostProcessor(InfoflowManager manager) {
-        this.manager = manager;
-    }
+	public ConditionalFlowPostProcessor(InfoflowManager manager) {
+		this.manager = manager;
+	}
 
-    @Override
-    public InfoflowResults onResultsAvailable(InfoflowResults results, IInfoflowCFG cfg) {
-        if (results.size() == 0)
-            return results;
+	@Override
+	public InfoflowResults onResultsAvailable(InfoflowResults results, IInfoflowCFG cfg) {
+		if (results.size() == 0)
+			return results;
 
-        logger.info("Filtering conditional flows...");
-        HashSet<ResultSinkInfo> tbr = new HashSet<>();
+		logger.info("Filtering conditional flows, starting with {} flows...", results.numConnections());
+		HashSet<ResultSinkInfo> tbr = new HashSet<>();
 
-        for (DataFlowResult dfRes : results.getResultSet()) {
-            Set<SourceSinkCondition> conditions = dfRes.getSink().getDefinition().getConditions();
-            // One of the conditions must match. Within both, a class name and a signature must match.
-            if (conditions != null && !conditions.isEmpty()
-                    && conditions.stream().noneMatch(cond -> cond.evaluate(dfRes, results)))
-                tbr.add(dfRes.getSink());
-        }
+		for (DataFlowResult dfRes : results.getResultSet()) {
+			Set<SourceSinkCondition> conditions = dfRes.getSink().getDefinition().getConditions();
+			// One of the conditions must match. Within both, a class name and a signature
+			// must match.
+			if (conditions != null && !conditions.isEmpty()
+					&& conditions.stream().noneMatch(cond -> cond.evaluate(dfRes, results)))
+				tbr.add(dfRes.getSink());
+		}
 
-        logger.info(String.format("Removed %d flows not meeting their conditions", tbr.size()));
-        results.removeAll(tbr);
+		logger.info(String.format("Removed %d flows not meeting their conditions", tbr.size()));
+		results.removeAll(tbr);
 
-        return results;
-    }
+		return results;
+	}
 }
