a7eaafc55f8445914f21f44143739253de0db854
Tim Lange <mail@tim-lange.me>
2023-04-17 12:02:19 +0200

    Remove test runner for to-be-removed test case of DroidBench

--- soot-infoflow-android/test/soot/jimple/infoflow/android/test/droidBench/backward/FieldSourceTest.java
+++
@@ -1,8 +0,0 @@
-package soot.jimple.infoflow.android.test.droidBench.backward;
-
-public class FieldSourceTest extends soot.jimple.infoflow.android.test.droidBench.FieldSourceTest {
-	@Override
-	protected TestResultMode getTestResultMode() {
-		return TestResultMode.FLOWDROID_BACKWARDS;
-	}
-}
