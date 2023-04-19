1f2bfb060f59868776f4bc37406411eeeb76c71b
Tim Lange <mail@tim-lange.me>
2023-04-19 13:28:56 +0200

    Hopefully the path is portable now

--- soot-infoflow-integration/test/soot/jimple/infoflow/integration/test/junit/AndroidRegressionTests.java
+++ soot-infoflow-integration/test/soot/jimple/infoflow/integration/test/junit/AndroidRegressionTests.java
@@ -7,11 +7,14 @@ import soot.jimple.infoflow.InfoflowConfiguration;
 import soot.jimple.infoflow.android.SetupApplication;
 import soot.jimple.infoflow.methodSummary.data.provider.EagerSummaryProvider;
 import soot.jimple.infoflow.methodSummary.taintWrappers.SummaryTaintWrapper;
+import soot.jimple.infoflow.methodSummary.taintWrappers.TaintWrapperFactory;
 import soot.jimple.infoflow.results.InfoflowResults;
 import soot.jimple.infoflow.taintWrappers.ITaintPropagationWrapper;
 
+import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Collections;
 
 /**
  * Tests that uncovered a bug.
@@ -20,8 +23,8 @@ public class AndroidRegressionTests extends BaseJUnitTests {
     @Override
     protected ITaintPropagationWrapper getTaintWrapper() {
         try {
-            return new SummaryTaintWrapper(new EagerSummaryProvider("../soot-infoflow-summaries/summariesManual"));
-        } catch (IOException | URISyntaxException e) {
+            return TaintWrapperFactory.createTaintWrapper(Collections.singleton("../soot-infoflow-summaries/summariesManual"));
+        } catch (IOException | XMLStreamException e) {
             throw new RuntimeException("Could not initialize taint wrapper!");
         }
     }
