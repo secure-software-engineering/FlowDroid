98b5c79e0298667e9eabf3063a6241b21b2f0ed5
Josef Ruffer <josef.ruffer@sit.fraunhofer.de>
2023-08-10 17:15:39 +0200

    provided default implementation

--- soot-infoflow-android/src/soot/jimple/infoflow/android/manifest/IService.java
+++ soot-infoflow-android/src/soot/jimple/infoflow/android/manifest/IService.java
@@ -1,5 +1,9 @@
 package soot.jimple.infoflow.android.manifest;
 
+import java.util.List;
+
+import soot.jimple.infoflow.android.entryPointCreators.AndroidEntryPointConstants;
+
 /**
  * Interface for services inside an Android app
  * 
@@ -8,4 +12,8 @@ package soot.jimple.infoflow.android.manifest;
  */
 public interface IService extends IAndroidComponent {
 
+	@Override
+	default public List<String> getLifecycleMethods() {
+		return AndroidEntryPointConstants.getServiceLifecycleMethods();
+	}
 }
