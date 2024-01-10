5946969eb3f58b7913e09dd4d0a0b3d11d01a2e2
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2024-01-10 23:43:47 +0100

    JavaDoc

--- soot-infoflow/src/soot/jimple/infoflow/callmappers/CallerCalleeManager.java
+++ soot-infoflow/src/soot/jimple/infoflow/callmappers/CallerCalleeManager.java
@@ -5,6 +5,12 @@ import soot.jimple.InvokeExpr;
 import soot.jimple.Stmt;
 import soot.jimple.infoflow.InfoflowManager;
 
+/**
+ * Looks up the responsible caller-callee argument mapper.
+ * See also {@link ICallerCalleeArgumentMapper}.
+ * 
+ * @author Marc Miltenberger
+ */
 public class CallerCalleeManager {
 
 	public static ICallerCalleeArgumentMapper getMapper(InfoflowManager manager, Stmt callSite, SootMethod callee) {
