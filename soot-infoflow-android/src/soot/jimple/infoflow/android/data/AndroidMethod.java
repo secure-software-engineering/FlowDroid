8b2a5a43e31d3a6044aecd3fa245539e2fac8bfc
Jordan Samhi <35679728+JordanSamhi@users.noreply.github.com>
2024-02-16 14:02:33 +0100

    Update: will move check to parent

--- soot-infoflow-android/src/soot/jimple/infoflow/android/data/AndroidMethod.java
+++ soot-infoflow-android/src/soot/jimple/infoflow/android/data/AndroidMethod.java
@@ -164,11 +164,6 @@ public class AndroidMethod extends SootMethodAndClass {
 			return false;
 		if (sourceSinkType != other.sourceSinkType)
 			return false;
-		if (!this.className.equals(other.className)
-		    || !this.methodName.equals(other.methodName)
-		    || !this.returnType.equals(other.returnType)) {
-			return false;
-		}
 		return true;
 	}
 
