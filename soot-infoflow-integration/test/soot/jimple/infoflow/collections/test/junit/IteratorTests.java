93afd46d010d5a9c321cc113e33ef1006cf60192
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2024-05-24 17:28:32 +0200

    added more summaries and more test cases, improved consistency in the summaries

--- soot-infoflow-integration/test/soot/jimple/infoflow/collections/test/junit/IteratorTests.java
+++ soot-infoflow-integration/test/soot/jimple/infoflow/collections/test/junit/IteratorTests.java
@@ -9,146 +9,154 @@ import soot.jimple.infoflow.IInfoflow;
 import soot.jimple.infoflow.InfoflowConfiguration;
 
 public class IteratorTests extends FlowDroidTests {
-    @Override
-    protected void setConfiguration(InfoflowConfiguration config) {
+	@Override
+	protected void setConfiguration(InfoflowConfiguration config) {
 
-    }
+	}
 
-    private static final String testCodeClass = "soot.jimple.infoflow.collections.test.IteratorTestCode";
+	private static final String testCodeClass = "soot.jimple.infoflow.collections.test.IteratorTestCode";
 
-    @Test(timeout = 30000)
-    public void testListSublist1() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testListSublist1() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testListSublist2() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testListSublist2() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testListIterator1() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testListIterator1() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testListIterator2() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testListIterator2() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testListListIterator1() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testListListIterator1() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testListListIterator2() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testListListIterator2() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator1() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator1() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator2() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator2() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator3() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator3() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator4() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator4() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator5() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator5() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator6() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator6() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator7() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator7() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator8() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator8() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator9() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator9() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator10() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator10() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 
-    @Test(timeout = 30000)
-    public void testIterator11() {
-        IInfoflow infoflow = initInfoflow();
-        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
-        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
-        Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
-    }
+	@Test(timeout = 30000)
+	public void testIterator11() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
+
+	@Test(timeout = 30000)
+	public void testSpliteratorFlow() {
+		IInfoflow infoflow = initInfoflow();
+		String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+		infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+		Assert.assertEquals(getExpectedResultsForMethod(epoint), infoflow.getResults().size());
+	}
 }
--- soot-infoflow-summaries/src/soot/jimple/infoflow/methodSummary/data/summary/GapDefinition.java
+++ soot-infoflow-summaries/src/soot/jimple/infoflow/methodSummary/data/summary/GapDefinition.java
@@ -1,6 +1,5 @@
 package soot.jimple.infoflow.methodSummary.data.summary;
 
-
 /**
  * Definition of a gap in a method summary. A gap occurs if a data flow reaches
  * a callback method which is not known to the library generation code, but must
@@ -10,76 +9,85 @@ package soot.jimple.infoflow.methodSummary.data.summary;
  *
  */
 public class GapDefinition {
-	
+
 	private final int id;
 	private String signature;
-	
+
 	/**
 	 * Creates a new instance of the {@link GapDefinition} class
-	 * @param id The unique ID of this gap definition
+	 * 
+	 * @param id        The unique ID of this gap definition
 	 * @param signature The signature of the callee
 	 */
 	public GapDefinition(int id, String signature) {
+		if (signature == null)
+			throw new IllegalArgumentException("Cannot have a gap without a signature");
 		this.id = id;
 		this.signature = signature;
 	}
-	
+
 	/**
 	 * Creates a new instance of the {@link GapDefinition} class
+	 * 
 	 * @param id The unique ID of this gap definition
 	 */
 	public GapDefinition(int id) {
 		this.id = id;
 	}
-	
+
 	/**
 	 * Gets the unique ID of this gap definition
+	 * 
 	 * @return The unique ID of this gap definition
 	 */
 	public int getID() {
 		return this.id;
 	}
-	
+
 	/**
 	 * Gets the signature of the callee of this gap
+	 * 
 	 * @return The signature of the callee of this gap
 	 */
 	public String getSignature() {
 		return this.signature;
 	}
-	
+
 	/**
 	 * Sets the signature of the callee of this gap
+	 * 
 	 * @param signature The signature of the callee of this gap
 	 */
 	public void setSignature(String signature) {
+		if (signature == null)
+			throw new IllegalArgumentException("Cannot have a gap without a signature");
 		this.signature = signature;
 	}
-	
+
 	@Override
 	public GapDefinition clone() {
 		return new GapDefinition(id, signature);
 	}
-	
+
 	/**
 	 * Generates a clone of this gap with a new ID
+	 * 
 	 * @param newID The new gap ID
 	 * @return The new gap with the changed ID
 	 */
 	public GapDefinition renumber(int newID) {
 		return new GapDefinition(newID, signature);
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + id;
-		result = prime * result
-				+ ((signature == null) ? 0 : signature.hashCode());
+		result = prime * result + ((signature == null) ? 0 : signature.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -98,10 +106,10 @@ public class GapDefinition {
 			return false;
 		return true;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Gap " + id + " in " + signature;
 	}
-	
+
 }
